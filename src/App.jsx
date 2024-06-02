2d900d6977509c35a388c74e4c3c23d3aeed199f
Jyotirmoy Sharma <sharmajyotirmoy44@gmail.com>
2024-06-02 18:07:31 +0530

    changing text color

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Technologies from "./components/Technologies"
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased
-    selection:bg-cyan-300 selection:text-cyan-600">
+    selection:bg-cyan-300 selection:text-green-700">
 
     <div className="fixed top-0 -z-10 h-full w-full">
 
